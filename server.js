fda436ae5cf996eb6e28ec574ca056f50aff63c9
Arielle Hein <ariellehein@gmail.com>
2014-12-03 20:32:23 -0500

    sends and loads images, working on creating gifs

--- server.js
+++ server.js
@@ -42,12 +42,44 @@ var currentImage = null;
 io.sockets.on('connection', // This is run for each individual user that connects
 	function (socket) {
 		console.log("We have a new client: " + socket.id);
-		//socket.emit(currentImage);
-		
+
+		// send client all existing images on index page connect
+		socket.on('fileRequest', function(data) {
+			console.log("INDEX PAGE CONNECTED!");
+
+			// read all the files in the folder in sequence, using callbacks
+			var fs = require("fs");
+
+			fs.readdir( "images", function(err, files) {
+			    if (err) {
+			        console.log("Error listing file contents.");
+			    } else {
+			    	
+			    	// var fileString = JSON.stringify(files);
+
+			    	// console.log(fileString);
+
+			    	socket.emit("files", files);
+			    }
+			});
+		});
+
+		//recieved image that was sent from camera.html - save this image to the server!
 		socket.on('image', function(data) {
 			currentImage = data;
 			console.log("Received: 'image' ");
 			io.sockets.emit('image',data);
+
+			var fs = require('fs');
+			var base64Data = data.replace(/^data:image\/webp;base64,/, "");
+
+			fs.writeFile("out.webp", base64Data, 'base64', function(err) {
+        		if (err) {
+                	console.log(err);
+        		} else {
+                	console.log("WRote image");
+        		}
+			});
 		});
 		
 		socket.on('disconnect', function() {
